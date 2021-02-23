cc4e2a704c5bc6a77e866c3c42b0e3566a4eea76
frank948 <nego94hp@gmail.com>
2021-02-23 15:21:59 -0500

    this is really bad

--- snake.py
+++ snake.py
@@ -2,8 +2,8 @@ import random
 import turtle
 import time
 
-score = 1
-high_score = 2
+score = 0
+high_score = 5
 
 s = turtle.Screen()
 s.setup(width=1000, height=1000)
@@ -42,7 +42,7 @@ snake.goto(0, 0)
 fruit = turtle.Turtle()
 fruit.speed(0)
 fruit.color("red")
-fruit.pensize(5)
+fruit.turtlesize(1)
 fruit.shape("circle")
 fruit.penup()
 fruit.goto(0, 100)
@@ -66,98 +66,177 @@ base = 0
 
 def move():
 
+    while base == 0:
+        time.sleep(1)
+        snake.goto(0, 0)
     while base == 3:
+        global score
+        score = 0
+        score_.clear()
+        score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                     font=('candara', 15, 'bold'))
         time.sleep(1)
         snake.goto(0, 0)
 
-    # print('working')
-
     while base == -1:
-        # print(base, "where am i")
         def left():
             x = snake.xcor()
             snake.setx(x - 10)
+            global score
+            global high_score
             if snake.distance(fruit) < 20:
-                x = random.randint(0, 200)
-                y = random.randint(0, 200)
+                x = random.randint(0, 350)
+                y = random.randint(0, 350)
                 fruit.goto(x, y)
-            # print((snake.xcor()), (snake.ycor()))
-            # print(base)
-            # print('left')
+
+
+                score += 1
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                if score > high_score:
+                    high_score = score
+                    print(high_score)
+                    score_.clear()
+                    score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                                 font=('candara', 15, 'bold'))
+            if snake.xcor() < -400 or snake.xcor() > 400 or snake.ycor() < -400 or snake.ycor() > 400:
+                score = 0
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                time.sleep(1)
+                snake.goto(0, 0)
+                global base
+                base = 0
+                return base
+
+
         left()
     while base == 1:
         # print(base, "where am i")
         def right():
             x = snake.xcor()
             snake.setx(x + 10)
+            global score
+            global high_score
             if snake.distance(fruit) < 20:
-                x = random.randint(0, 200)
-                y = random.randint(0, 200)
+                x = random.randint(0, 350)
+                y = random.randint(0, 350)
                 fruit.goto(x, y)
-            # print(base)
-            # print((snake.xcor()), (snake.ycor()))
-            # print('right')
+
+
+                score += 1
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                if score > high_score:
+                    high_score = score
+                    print(high_score)
+                    score_.clear()
+                    score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                                 font=('candara', 15, 'bold'))
+            if snake.xcor() < -400 or snake.xcor() > 400 or snake.ycor() < -400 or snake.ycor() > 400:
+                score = 0
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                time.sleep(1)
+                snake.goto(0, 0)
+                global base
+                base = 0
+                return base
+
         right()
     while base == -2:
-        # print(base, "where am i")
         def down():
             y = snake.ycor()
             snake.sety(y - 10)
+            global score
+            global high_score
             if snake.distance(fruit) < 20:
-                x = random.randint(0, 200)
-                y = random.randint(0, 200)
+                x = random.randint(0, 350)
+                y = random.randint(0, 350)
                 fruit.goto(x, y)
 
-            # print(base)
-            # print((snake.xcor()), (snake.ycor()))
-            # print('down')
+
+                score += 1
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                if score > high_score:
+                    high_score = score
+                    print(high_score)
+                    score_.clear()
+                    score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                                 font=('candara', 15, 'bold'))
+            if snake.xcor() < -400 or snake.xcor() > 400 or snake.ycor() < -400 or snake.ycor() > 400:
+                score = 0
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                time.sleep(1)
+                snake.goto(0, 0)
+                global base
+                base = 0
+                return base
+
         down()
     while base == 2:
-        # print(base, "where am i")
         def up():
             y = snake.ycor()
             snake.sety(y + 10)
+            global score
+            global high_score
             if snake.distance(fruit) < 20:
-                x = random.randint(0, 200)
-                y = random.randint(0, 200)
+                x = random.randint(0, 350)
+                y = random.randint(0, 350)
                 fruit.goto(x, y)
-            # print(base)
-            # print((snake.xcor()), (snake.ycor()))
-            # print('up')
+                
+                score += 1
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                if score > high_score:
+                    high_score = score
+                    print(high_score)
+                    score_.clear()
+                    score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                                     font=('candara', 15, 'bold'))
+            if snake.xcor() < -400 or snake.xcor() > 400 or snake.ycor() < -400 or snake.ycor() > 400:
+                score = 0
+                score_.clear()
+                score_.write("Score : {} High Score: {}".format(score, high_score), align='center',
+                             font=('candara', 15, 'bold'))
+                time.sleep(1)
+                snake.goto(0, 0)
+                global base
+                base = 0
+                return base
         up()
-
 def go_left():
     global base
     base = -1
-    # print(base, 'left')
     move()
-
 def go_right():
     global base
     base = 1
-    # print(base, 'right')
     move()
-
 def go_down():
     global base
     base = -2
-    # print(base, 'down')
     move()
 
 def go_up():
     global base
     base = 2
-    # print(base, 'up')
     move()
-
 def reset_():
     global base
     base = 3
     move()
-
 def done_():
     s.bye()
-
 
 s.listen()
 s.onkeypress(go_up, 'Up')
@@ -170,14 +249,8 @@ s.onkeypress(done_, 'e')
 
 
 s.update()
-print('where is this')
+
 if (snake.xcor() < -397):
     print('lose')
-
-if snake.distance(fruit) < 50:
-    print('where')
-
-
 move()
-
 s.mainloop()
